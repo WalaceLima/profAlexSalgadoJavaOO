ff41cd086fc954ed2becdb0a69b3e5dc575042e4
Walace Lima <walace.lima@soulasalle.com.br>
2021-10-29 08:22:04 -0300

    Criação da calculadora

---
+++ entities/Palavra.java
@@ -0,0 +1,17 @@
+package entities;
+
+import java.util.Scanner;
+
+public class Palavra {
+  
+try {
+    String nome;
+    System.out.println("Entre com seu nome: ");
+    Scanner rec= new Scanner(System.in);
+    
+} catch (Exception e) {
+  System.out.println("resultado não permitido!");
+    }
+} 
+
+
